52b15cb661dbc8153f386594542549d323744e2c
roanacla <roanacla@me.com>
2020-06-10 12:43:50 -0700

    Create custom images using Drawing techniques

--- MountainAirport/FirstVisitAward.swift
+++ MountainAirport/FirstVisitAward.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2019 Razeware LLC
-///
+/// Copyright (c) 2020 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,26 +28,42 @@
 
 import SwiftUI
 
-struct AirportAwards: View {
-  var body: some View {
-    VStack {
-      ScrollView {
-        FirstVisitAward()
-          .frame(width: 250, height: 250)
-        Text("First Visit")
-        OverNightParkAward()
-          .frame(width: 250, height: 250)
-        Text("Left Car Overnight")
-        AirportMealAward()
-          .frame(width: 250, height: 250)
-        Text("Ate Meal at Airport")
-      }
-    }.navigationBarTitle("Your Awards")
-  }
+struct FirstVisitAward: View {
+    var body: some View {
+        GeometryReader { geometry in
+          ZStack {
+            ForEach(0..<3) { i in
+              Rectangle()
+                .fill(
+                  LinearGradient(
+                    gradient: .init(colors: [Color.green, Color.blue]),
+                    startPoint: .init(x: 0, y: 1),
+                    endPoint: .init(x: 1, y: 0)
+                  )
+                )
+                .frame(width: geometry.size.width * 0.7,
+                     height: geometry.size.width * 0.7)
+              .rotationEffect(.degrees(Double(i) * 60.0))
+            }
+            Image(systemName: "airplane")
+              .resizable().rotationEffect(.degrees(-90))
+              .opacity(0.5)
+              .scaleEffect(0.7)
+          }
+        }
+    }
 }
 
-struct AirportAwards_Previews: PreviewProvider {
+struct FirstVisitAward_Previews: PreviewProvider {
   static var previews: some View {
-    AirportAwards()
+    Group {
+      FirstVisitAward()
+        .environment(\.colorScheme, .light)
+        .frame(width: 200, height: 200)
+
+      FirstVisitAward()
+        .environment(\.colorScheme, .dark)
+        .frame(width: 200, height: 200)
+    }
   }
 }
